840da833b06788bb24cc8cdfc75947a4f682ee2a
Eugen Ciur <ciur.eugen@gmail.com>
2025-04-27 12:57:15 +0200

    version bump 0.3 -> 0.4

--- envok.go
+++ envok.go
@@ -8,7 +8,7 @@ import (
 	"github.com/ciur/envok/commands"
 )
 
-const VERSION = "0.3"
+const VERSION = "0.4"
 
 var profileName = flag.String("p", "", "Profile name")
 var version = flag.Bool("v", false, "show version and exit")
