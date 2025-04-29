b742c9a64002e5b341d4f383f98f6627196a5c35
Eugen Ciur <ciur.eugen@gmail.com>
2025-04-29 21:29:37 +0200

    it works!

---
+++ changelog.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## [0.7.3] - 2025-04-29
+
+### Adds
+
+- Bugfix: besides printing export also print "unset" to undo the
+  value of previously set environment variable.
--- commands/export.go
+++ commands/export.go
@@ -9,6 +9,9 @@ import (
 )
 
 func ExportProfile(defaultConfigPath *string, name string) {
+	var exportedKeys []string
+	var keysToUnset []string
+
 	configPath, err := getConfigPath(defaultConfigPath)
 
 	if err != nil {
@@ -36,7 +39,44 @@ func ExportProfile(defaultConfigPath *string, name string) {
 			// iterate profile.Vars with keys sorted
 			for _, k := range keys {
 				fmt.Printf("export %s=%s\n", k, profile.Vars[k])
+				exportedKeys = append(exportedKeys, k)
 			}
 		}
 	}
+
+	for _, profile := range items {
+		for k := range profile.Vars {
+			if !contains(exportedKeys, k) {
+				keysToUnset = append(keysToUnset, k)
+			}
+		}
+	}
+
+	keysToUnset = uniqueStrings(keysToUnset)
+	for _, item := range keysToUnset {
+		fmt.Printf("unset %s\n", item)
+	}
+}
+
+func uniqueStrings(input []string) []string {
+	seen := make(map[string]struct{}) // use an empty struct because it uses no memory
+	var result []string
+
+	for _, str := range input {
+		if _, exists := seen[str]; !exists {
+			seen[str] = struct{}{}
+			result = append(result, str)
+		}
+	}
+
+	return result
+}
+
+func contains(list []string, target string) bool {
+	for _, item := range list {
+		if item == target {
+			return true
+		}
+	}
+	return false
 }
